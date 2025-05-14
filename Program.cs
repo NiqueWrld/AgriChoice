c33eaeb0d2777afb9cfb5f9db0a3495f38d8b2a7
Niquewrld <119890333+NiqueWrld@users.noreply.github.com>
2025-05-14 05:54:21 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using AgriChoice.Data;
 using Braintree;
 
-var builder = WebApplication.CreateBuilder(args);
+var builde = WebApplication.CreateBuilder(args);
 
 // Get the connection string from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("AgriChoiceContextConnection")
